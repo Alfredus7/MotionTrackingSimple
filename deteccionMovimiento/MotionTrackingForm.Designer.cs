67edccb3457b13b0155716e7a91de797e245a558
Freddy Avalos Pardo <apf4003748@est.univalle.edu>
2025-06-14 12:54:38 -0400

    Agregar archivos de proyecto.

---
+++ deteccionMovimiento/MotionTrackingForm.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace deteccionMovimiento
+{
+    partial class MotionTrackingForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MotionTrackingForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cargarGIFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarGifToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBoxOriginal = new System.Windows.Forms.PictureBox();
+            this.pictureBoxProcesada = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProcesada)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cargarGIFToolStripMenuItem,
+            this.guardarGifToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "opciones";
+            // 
+            // cargarGIFToolStripMenuItem
+            // 
+            this.cargarGIFToolStripMenuItem.Name = "cargarGIFToolStripMenuItem";
+            this.cargarGIFToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.cargarGIFToolStripMenuItem.Text = "cargar GIF";
+            this.cargarGIFToolStripMenuItem.Click += new System.EventHandler(this.CargarGIF_Click);
+            // 
+            // guardarGifToolStripMenuItem
+            // 
+            this.guardarGifToolStripMenuItem.Name = "guardarGifToolStripMenuItem";
+            this.guardarGifToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.guardarGifToolStripMenuItem.Text = "guardarGif";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBoxOriginal, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBoxProcesada, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 71.3615F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 426);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBoxOriginal
+            // 
+            this.pictureBoxOriginal.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxOriginal.BackgroundImage")));
+            this.pictureBoxOriginal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxOriginal.Location = new System.Drawing.Point(3, 55);
+            this.pictureBoxOriginal.Name = "pictureBoxOriginal";
+            this.pictureBoxOriginal.Size = new System.Drawing.Size(394, 368);
+            this.pictureBoxOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxOriginal.TabIndex = 2;
+            this.pictureBoxOriginal.TabStop = false;
+            // 
+            // pictureBoxProcesada
+            // 
+            this.pictureBoxProcesada.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxProcesada.BackgroundImage")));
+            this.pictureBoxProcesada.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxProcesada.Location = new System.Drawing.Point(403, 55);
+            this.pictureBoxProcesada.Name = "pictureBoxProcesada";
+            this.pictureBoxProcesada.Size = new System.Drawing.Size(394, 368);
+            this.pictureBoxProcesada.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxProcesada.TabIndex = 1;
+            this.pictureBoxProcesada.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 52);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Imagen Original";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(403, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(394, 52);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Imagen Prosesada";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 407);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(800, 43);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "opciones";
+            // 
+            // MotionTrackingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MotionTrackingForm";
+            this.Text = "Motion Tracking";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProcesada)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cargarGIFToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBoxProcesada;
+        private System.Windows.Forms.PictureBox pictureBoxOriginal;
+        private System.Windows.Forms.ToolStripMenuItem guardarGifToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ deteccionMovimiento/MotionTrackingForm.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using Accord.Imaging.Filters;
+
+namespace deteccionMovimiento
+{
+    /// <summary>
+    /// Formulario principal para el seguimiento de movimiento simple
+    /// Detecta y sigue un único objeto en movimiento en un GIF animado
+    /// </summary>
+    public partial class MotionTrackingForm : Form
+    {
+        // Almacena los frames originales del GIF cargado
+        private List<Bitmap> originalFrames = new List<Bitmap>();
+
+        // Guarda la trayectoria del objeto detectado (coordenadas X,Y)
+        private List<Point> trayectoria = new List<Point>();
+
+        // Índice del frame actual que se está procesando
+        private int gifFrameIndex = 0;
+
+        // Temporizador para controlar la animación
+        private Timer timer = new Timer { Interval = 100 }; // 10 FPS (100ms por frame)
+
+        /// <summary>
+        /// Constructor del formulario
+        /// </summary>
+        public MotionTrackingForm()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true; // Para renderizado suave
+            timer.Tick += (s, e) => ProcesarFrame();
+        }
+
+        /// <summary>
+        /// Evento del botón para cargar un GIF animado
+        /// </summary>
+        private void CargarGIF_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog
+            {
+                Filter = "GIF animado (*.gif)|*.gif",
+                Title = "Seleccione un GIF animado para analizar"
+            })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    CargarGIF(ofd.FileName);
+                    gifFrameIndex = 0;
+                    trayectoria.Clear();
+                    timer.Start();
+                }
+            }
+            groupBox1.Enabled = true;
+        }
+
+        /// <summary>
+        /// Carga un archivo GIF y extrae todos sus frames
+        /// </summary>
+        /// <param name="path">Ruta del archivo GIF a cargar</param>
+        private void CargarGIF(string path)
+        {
+            // Limpiar frames anteriores
+            originalFrames.ForEach(frame => frame?.Dispose());
+            originalFrames.Clear();
+
+            using (var gif = Accord.Imaging.Image.FromFile(path))
+            {
+                var dimension = new FrameDimension(gif.FrameDimensionsList[0]);
+                int frameCount = gif.GetFrameCount(dimension);
+
+                // Extraer cada frame del GIF
+                for (int i = 0; i < frameCount; i++)
+                {
+                    gif.SelectActiveFrame(dimension, i);
+                    var frame = new Bitmap(gif.Width, gif.Height);
+                    using (var g = Graphics.FromImage(frame))
+                        g.DrawImage(gif, 0, 0);
+                    originalFrames.Add(frame);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Procesa el frame actual: detecta movimiento y actualiza la visualización
+        /// </summary>
+        private void ProcesarFrame()
+        {
+            if (originalFrames.Count == 0) return;
+
+            // Obtener frame original actual
+            var original = originalFrames[gifFrameIndex];
+            pictureBoxOriginal.Image = original;
+
+            // Procesar imagen para detección de movimiento
+            var processed = AplicarFiltros(original);
+
+            // Calcular centroide del objeto en movimiento
+            var centroide = CalcularCentroide(processed);
+
+            // Registrar posición en la trayectoria (si se detectó objeto)
+            if (!centroide.IsEmpty) trayectoria.Add(centroide);
+
+            // Dibujar trayectoria y marcadores
+            using (var g = Graphics.FromImage(processed))
+            {
+                // Dibujar trayectoria histórica
+                if (trayectoria.Count > 1)
+                {
+                    for (int i = 1; i < trayectoria.Count; i++)
+                    {
+                        // Línea conectando puntos anteriores
+                        g.DrawLine(Pens.GreenYellow, trayectoria[i - 1], trayectoria[i]);
+                        // Puntos pequeños verdes para posiciones anteriores
+                        g.FillEllipse(Brushes.DarkGreen, trayectoria[i].X - 3, trayectoria[i].Y - 3, 6, 6);
+                    }
+                }
+
+                // Dibujar posición actual (si hay detección)
+                if (!centroide.IsEmpty)
+                {
+                    // Punto rojo grande para posición actual
+                    g.FillEllipse(Brushes.Red, centroide.X - 5, centroide.Y - 5, 10, 10);
+                    // Círculo exterior para mejor visibilidad
+                    g.DrawEllipse(Pens.DarkRed, centroide.X - 7, centroide.Y - 7, 14, 14);
+                }
+            }
+
+            // Actualizar PictureBox con el resultado procesado
+            pictureBoxProcesada.Image?.Dispose();
+            pictureBoxProcesada.Image = processed;
+
+            // Avanzar al siguiente frame (cíclico)
+            gifFrameIndex = (gifFrameIndex + 1) % originalFrames.Count;
+        }
+
+        /// <summary>
+        /// Aplica filtros a la imagen para resaltar el objeto en movimiento
+        /// </summary>
+        /// <param name="original">Imagen original a procesar</param>
+        /// <returns>Imagen binaria procesada</returns>
+        private Bitmap AplicarFiltros(Bitmap original)
+        {
+            // Convertir a escala de grises (ponderación estándar)
+            Bitmap gris = new Grayscale(0.2125, 0.7154, 0.0721).Apply(original);
+
+            // Invertir colores (para que el objeto sea blanco)
+            new Invert().ApplyInPlace(gris);
+
+            // Umbralización automática (Otsu)
+            new OtsuThreshold().ApplyInPlace(gris);
+
+            // Dilatación para unir áreas cercanas
+            new Dilation().ApplyInPlace(gris);
+
+            // Convertir a formato RGB estándar
+            Bitmap result = new Bitmap(gris.Width, gris.Height, PixelFormat.Format24bppRgb);
+            using (Graphics g = Graphics.FromImage(result))
+                g.DrawImage(gris, 0, 0);
+
+            gris.Dispose();
+            return result;
+        }
+
+        /// <summary>
+        /// Calcula el centroide (centro de masa) de los píxeles blancos
+        /// </summary>
+        /// <param name="bmp">Imagen binaria (blanco=objeto)</param>
+        /// <returns>Punto con las coordenadas del centroide o Point.Empty si no se detecta objeto</returns>
+        private Point CalcularCentroide(Bitmap bmp)
+        {
+            unsafe
+            {
+                int sumX = 0, sumY = 0, count = 0;
+
+                // Recorrer toda la imagen buscando píxeles blancos (R=255)
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    for (int x = 0; x < bmp.Width; x++)
+                    {
+                        if (bmp.GetPixel(x, y).R == 255) // Píxel blanco detectado
+                        {
+                            sumX += x; // Acumular coordenada X
+                            sumY += y; // Acumular coordenada Y
+                            count++;   // Contar píxeles del objeto
+                        }
+                    }
+                }
+
+                // Devolver punto promedio (centroide) o vacío si no hay detección
+                return count > 0 ? new Point(sumX / count, sumY / count) : Point.Empty;
+            }
+        }
+    }
+}
